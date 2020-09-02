a7a9a10a035d012a04d858aa9c0ae2dd444d2a0e
Everton Afonso <54124207+Everton-Afonso@users.noreply.github.com>
2020-09-02 09:04:42 -0300

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div id="cabe" className="jumbotron">
+      <h1 className="display-4">Sistemas de Informação sobre Convid 19</h1>
+      <p className="lead">Lista de Casos e Morte no Brasil por estado</p>
+    </div>
+  );
+}
+
+export default Header;
